7811e58320d6de577a09fa59a97212f5c697141c
Jonatan Jesús Fuentes Hernández <jonatanjfh24101994@ciencias.unam.mx>
2026-02-28 19:20:37 -0600

    Agregando la practica02-3D de laboratorio y también avances de la practica02 de ayudantía

---
+++ Ayudantia/Practica02/Proyecto2D/Assets/Scripts/ControlJugador.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class ControlJugador : MonoBehaviour
+{
+    //Variables para velocidad, aceleración, gravedad y estado vertical.
+    public float velocidadActual = 0f;
+    public float velocidadMax = 5f;
+    public float aceleración =  10f;
+    public float velocidadVertical = 0f;
+    public float gravedad = -20f;
+    public float tiempoMaxSalto = 0.2f;
+    private float tiempoSaltoActual = 0f;
+    private float tiempoAnterior;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Calculo manual de deltaTime.
+        float delta = Time.time - tiempoAnterior;
+        tiempoAnterior = Time.time;
+
+        //Movimiento lateral con aceleración.
+        float h = Input.GetAxis("Horizontal");
+        velocidadActual += h * aceleración * delta;
+        velocidadActual  = Mathf.Clamp(velocidadActual, - velocidadMax, velocidadMax);
+        transform.position += new Vector3(velocidadActual * delta, 0, 0);
+
+        //Salto
+        if(Input.GetAxis("Jump") > 0  && jugador.enSuelo)
+        {
+            velocidadVertical = 10f;
+            jugador.enSuelo = false;
+            tiempoSaltoActual = 0f;
+        }
+        if(!jugador.enSuelo && Imput.GetAxis("Jump") > 0)
+        {
+            if(tiempoSaltoActual < tiempoMaxSalto)
+            {
+                velocidadVertical += 20f * delta;
+                tiempoSaltoActual *= delta;
+            }
+        }
+        if(Input.GetAxis("Jump") == 0)
+        {
+            tiempoSaltoActual = tiempoMaxSalto;
+        }
+        if(jugador.enSuelo)
+        {
+            velocidadVertical = 0;
+        }
+        else
+        {
+            velocidadVertical += gravedad * delta;
+        }
+        transform.position += new Vector3(0,velocidadVertical * delta, 0);
+    }
+}
---
+++ Laboratorio/Practica02_3D/Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickToMove : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out RaycastHit hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }    
+    }
+}
