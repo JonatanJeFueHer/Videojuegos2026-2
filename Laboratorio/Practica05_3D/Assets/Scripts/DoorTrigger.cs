e9e76c13bc64245f5722784e53c307a9b6229299
Jonatan Jesús Fuentes Hernández <jonatanjfh24101994@ciencias.unam.mx>
2026-03-25 12:18:54 -0600

    Practica05_3D: Push inicial.

---
+++ Laboratorio/Practica05_3D/Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System;
+public class DoorTrigger : MonoBehaviour
+{
+    public event Action OnDoorOpen;
+    public event Action OnDoorClose;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            OnDoorOpen?.Invoke();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            OnDoorClose?.Invoke();
+        }
+    }
+}
