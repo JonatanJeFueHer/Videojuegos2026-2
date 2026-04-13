12ed4f0d44b1548eb7b74f8dcf71e0deffbf1592
Jonatan Jesús Fuentes Hernández <jonatanjfh24101994@ciencias.unam.mx>
2026-04-12 19:09:09 -0600

    Practica06_3D: Solucionando problemas de movimiento.

--- Laboratorio/Practica06_3D/Assets/Scripts/Player/PlayerMovement.cs
+++ Laboratorio/Practica06_3D/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,29 +1,53 @@
-using UnityEngine;
-public class PlayerMovement : MonoBehaviour {
+﻿using UnityEngine;
+public class PlayerMovement : MonoBehaviour
+{
     public float speed = 6f;
     public float runSpeed = 10f;
     public float gravity = -9.8f;
     public float jumpForce = 8f;
     CharacterController controller;
     Vector3 velocity;
-    void Start(){
+
+    public Transform cameraTransform;
+    void Start()
+    {
         controller = GetComponent<CharacterController>();
     }
-void Update()
-{
-    float x = Input.GetAxis("Horizontal");
-    float z = Input.GetAxis("Vertical");
-    Vector3 move = transform.right * x + transform.forward * z;
-    bool isRunning = Input.GetKey(KeyCode.LeftShift);
-    float currentSpeed = isRunning ? runSpeed : speed;
-    controller.Move(move * currentSpeed * Time.deltaTime);
-    if(controller.isGrounded && velocity.y < 0)
-        velocity.y = -2f;
-    if(Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
-        velocity.y = jumpForce;
-    velocity.y += gravity * Time.deltaTime;
-    controller.Move(velocity * Time.deltaTime);
- }
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //Codigo viejo: moverse respecto al sistema de coordenadas local del jugador
+        //Vector3 move = transform.right * x + transform.forward * z;
+
+
+
+        //Codigo nuevo
+
+        //Moverse respecto al sistema de coordenadas global
+        // Vector3 move = new Vector3(x,0,z);
+
+        //Moverse respecto a la camara
+        Vector3 camForward = cameraTransform.forward;
+        Vector3 camRight = cameraTransform.right;
+        camForward.y = 0f;
+        camRight.y = 0f;
+        camForward.Normalize();
+        camRight.Normalize();
+        Vector3 move = camForward * z + camRight * x;
+
+
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float currentSpeed = isRunning ? runSpeed : speed;
+        controller.Move(move * currentSpeed * Time.deltaTime);
+        if (controller.isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+        if (Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
+            velocity.y = jumpForce;
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
 
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
--- Laboratorio/Practica06_3D/Assets/Scripts/Player/PlayerMovementAnim.cs
+++ Laboratorio/Practica06_3D/Assets/Scripts/Player/PlayerMovementAnim.cs
@@ -1,11 +1,8 @@
 using UnityEngine;
-
 public class PlayerMovementAnim : MonoBehaviour
 {
-    public Transform cameraTransform;
     Animator anim;
     CharacterController controller;
-
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -15,19 +12,7 @@ public class PlayerMovementAnim : MonoBehaviour
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        Vector3 camForward = cameraTransform.forward;
-        Vector3 camRight = cameraTransform.right;
-        Vector3 moveDir = camForward * z + camRight * x;
-        float speedValue = moveDir.magnitude;
-        anim.SetFloat("Speed", speedValue);
-            
-
-        camForward.y = 0f;
-        camRight.y = 0f;
-
-        camForward.Normalize();
-        camRight.Normalize();
-
+        float speedValue = new Vector2(x, z).magnitude;
         anim.SetFloat("Speed", speedValue);
         Vector3 direction = new Vector3(x, 0, z);
         if (direction.magnitude > 0.1f)
@@ -45,16 +30,6 @@ public class PlayerMovementAnim : MonoBehaviour
         else
         {
             anim.SetBool("IsJumping", false);
-        }
-
-        if (moveDir.magnitude > 0.1f)
-        {
-            Quaternion targetRotation = Quaternion.LookRotation(moveDir);
-            transform.rotation = Quaternion.Slerp(
-            transform.rotation,
-            targetRotation,
-            Time.deltaTime * 10f
-            );
         }
     }
 }
